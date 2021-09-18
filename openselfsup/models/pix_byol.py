781356dc323d14aff92066a031cd125d61d58b65
ChengShuaiLin <1106982578@qq.com>
2021-09-18 11:09:34 +0800

    refine pix-byol

--- openselfsup/models/pix_byol.py
+++ openselfsup/models/pix_byol.py
@@ -1,7 +1,7 @@
 '''
 Author: Shuailin Chen
 Created Date: 2021-09-10
-Last Modified: 2021-09-10
+Last Modified: 2021-09-18
 	content: 
 '''
 
@@ -18,3 +18,16 @@ from .byol import BYOL
 class PixBYOL(BYOL):
     ''' BYOL on pixel level, contrasting pixel with mask embeddigns
     '''
+    
+    def __init__(self,
+                backbone,
+                neck=None,
+                head=None,
+                pretrained=None,
+                base_momentum=0.996,
+                **kwargs):
+
+        super().__init__()
+
+
+    def 
