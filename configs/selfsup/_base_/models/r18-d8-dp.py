7b111a0f14096eb5cd69020518e98ab39aafc3e3
chenda <1106982578@qq.com>
2021-10-22 18:50:01 +0800

    add resnet with stochastic depth

---
+++ configs/selfsup/_base_/models/r18-d8-dp.py
@@ -0,0 +1,15 @@
+'''
+Author: Shuailin Chen
+Created Date: 2021-09-30
+Last Modified: 2021-10-22
+	content: resnet with stochastic path
+'''
+
+_base_=['./r18-d8.py']
+
+model = dict(
+    backbone=dict(
+        type='ResNetDropPathV1c',
+        drop_path_ratio=0.3
+        ),
+)
--- openselfsup/models/backbones/__init__.py
+++ openselfsup/models/backbones/__init__.py
@@ -1,8 +1,9 @@
 '''
 Author: Shuailin Chen
 Created Date: 2021-09-14
-Last Modified: 2021-09-27
+Last Modified: 2021-10-22
 	content: 
 '''
 
 from .resnet import ResNetV1c, ResNet
+from .resnet_stochasitc_path import ResNetDropPath, ResNetDropPathV1c
